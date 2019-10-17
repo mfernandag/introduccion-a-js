a7f9e994719be2fc4703832a6205fafec9a117c7
mfernandag <mfernandagoracy@gmail.com>
2019-10-17 17:26:18 -0300

    tarea 2, chequear estilos

---
+++ tareas-resueltas/clase-5/tarea2.js
@@ -0,0 +1,31 @@
+// creá un formulario que capture el primer nombre, segundo nombre, apellido/s y edad del usuario
+// también vamos a crear un <h1> que diga Bienvenido!
+// vas a crear un botón de acción que una vez que lo apretás, va a
+// mostrar toda la información junta en un campo de texto
+// Y va a cambiar el <h1> para decir "Bienvenido, nombreDeUsuario"!
+/*
+Ejemplo form:
+  <form id="entrada-bar" onsubmit="return false;">
+    <input type="text" placeholder="Ingresá tu nombre" id="nombre-usuario"/>
+    <input type="number" placeholder="Ingresá tu edad" id="edad-usuario" />
+    <input type="submit" value="Ingresar" id="ingresar"/>
+  </form>
+
+  <div id="resultado">Hola</div>
+*
+* */
+
+document.querySelector('#submitBtn').onclick = function (){
+  const nombreCompleto = document.querySelector('#nombre').value;
+  const apellido = document.querySelector('#apellido').value;
+  const edad = document.querySelector('#edad').value;
+  const todo = 
+  `${nombreCompleto}
+${apellido}
+${edad} años`
+  document.querySelector('#textarea').value = todo;
+  const saludoContainer = document.querySelector('#saludoContainer')
+  let saludo = document.createElement('h1')
+  saludo.textContent = `¡Bienvenidx, ${nombreCompleto}!`
+  saludoContainer.appendChild(saludo)
+}
